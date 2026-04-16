15dcf793ea0f7186d45f58042e50ebf98f36cdf8
2004-tanu <tanuja.lokhande.2023@vpkbiet.org>
2026-04-16 08:14:16 -0400

    Added visualization in main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# Simple Data Science Example
+# Feature: Visualization
 
-import numpy as np
+import matplotlib.pyplot as plt
 
-data = np.array([1, 2, 3, 4, 5])
+data = [1, 2, 3, 4, 5]
 
-print("Mean:", np.mean(data))
-print("Sum:", np.sum(data))
+plt.plot(data)
+plt.title("Visualization")
+plt.show()
