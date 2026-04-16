a3aaae42bbd9365f412016326ab529604530018d
2004-tanu <tanuja.lokhande.2023@vpkbiet.org>
2026-04-16 08:06:52 -0400

    Added median calculation in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# Simple Data Science Example
+# Feature: Analysis
 
 import numpy as np
 
 data = np.array([1, 2, 3, 4, 5])
 
 print("Mean:", np.mean(data))
-print("Sum:", np.sum(data))
+print("Median:", np.median(data))
